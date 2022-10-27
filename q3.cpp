1d6d6533ff1131ffdea185ff8b92369c7b95f293
Aditya Paul <62583380+adityapaul18@users.noreply.github.com>
2022-10-27 11:57:17 +0530

    bug fix q3

--- q3.cpp
+++ q3.cpp
@@ -2,30 +2,56 @@
 using namespace std;
 #define ll long long int
 ll a=0;
-void get(ll t,ll c, vector<vector<ll>> &ans,ll th,ll p ,ll h){
+void get(ll t, ll c, vector<vector<ll>> &ans,ll th,ll p ,ll h){
 
     // cout<<t<<" "<<c<<" "<<th<<" "<<p<<" "<<h<<"\n";
-    if(t<0 || c<0)
+    a=max(a,c);
+    if(t<0)
         return;
-    else if(c==0){
+    else if(t==0){
+        // ans[c].push_back({th,p,h});
+        return;
+    }
+
+    if(t>=5)
+        get(t-5,c+(t-5)*1500,ans,th+1,p,h);
+    if(t>=4)
+        get(t-4,c+(t-4)*1000,ans,th,p+1,h);
+    if(t>=10)
+        get(t-10,c+(t-10)*3000,ans,th,p,h+1);
+
+}
+
+void soln(ll t, ll c, vector<vector<ll>> &ans,ll th,ll p ,ll h){
+    
+    // cout<<t<<" "<<c<<" "<<th<<" "<<p<<" "<<h<<"\n";
+    if(t<0)
+        return;
+
+    if(c==a){
         ans.push_back({th,p,h});
         return;
     }
 
-    get(t-5,c-(t-5)*1500,ans,th+1,p,h);
-    get(t-4,c-(t-4)*1000,ans,th,p+1,h);
-    get(t-10,c-(t-10)*3000,ans,th,p,h+1);
+    if(t>=5)
+        soln(t-5,c+(t-5)*1500,ans,th+1,p,h);
+    if(t>=4)
+        soln(t-4,c+(t-4)*1000,ans,th,p+1,h);
+    if(t>=10)
+        soln(t-10,c+(t-10)*3000,ans,th,p,h+1);
 
 }
 
 void answer(){
     ll t,c;
-    cin>>t>>c;
+    cin>>t;
 
     vector<vector<ll>> ans;
 
-    get(t,c,ans,0,0,0);
+    get(t,0,ans,0,0,0);
+    soln(t,0,ans,0,0,0);
 
+    cout<<"Earnings : "<<a<<" \n";
     for(auto k : ans){
         cout<<"T : "<<k[0]<<" "<<"P : "<<k[1]<<" "<<"C : "<<k[2]<<" \n";
     }
